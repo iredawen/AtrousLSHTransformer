41426da648b6c4d53b4f43e7d5396fe67588aa74
da <dawenxun@163.com>
2022-05-12 17:22:40 +0800

    预训练模型测试

--- tools/train.py
+++ tools/train.py
@@ -23,7 +23,7 @@ from mmdet.utils import (collect_env, get_device, get_root_logger,
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('config', help='train config file path')
+    parser.add_argument('--config', default='/home/dl4/x/AtrousLSHTransformer/configs/pvt/retinanet_pvt-t_fpn_1x_coco.py')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
         '--resume-from', help='the checkpoint file to resume from')
